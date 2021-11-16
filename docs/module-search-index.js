63f4264ce1dea70b6ff263492d45aea8f005a006
Jeremiah Osborne <alexosborne54@gmail.com>
2021-11-16 14:44:29 -0500

    * Republished JavaDocs
    Signed-off-by: Jeremiah Osborne <alexosborne54@gmail.com>

---
+++ docs/module-search-index.js
@@ -0,0 +1,5 @@
+/*
+ * Copyright (c) 2021 XZot1K, All rights reserved.
+ */
+
+moduleSearchIndex = [];updateSearchResults();
---
+++ docs/tag-search-index.js
@@ -0,0 +1,5 @@
+/*
+ * Copyright (c) 2021 XZot1K, All rights reserved.
+ */
+
+tagSearchIndex = [{"l":"Serialized Form","h":"","u":"serialized-form.html"}];updateSearchResults();
