c96dfd9b4083d8ae7ef27e61af7f1e8b24e4ce54
XZot1K <alexosborne54@gmail.com>
2023-09-04 15:58:08 -0400

    **Build 22** * Logging adjustments. * Appearance menu fixes.
    Signed-off-by: XZot1K <alexosborne54@gmail.com>

--- src/main/java/xzot1k/plugins/ds/api/enums/EditType.java
+++ src/main/java/xzot1k/plugins/ds/api/enums/EditType.java
@@ -5,5 +5,23 @@
 package xzot1k.plugins.ds.api.enums;
 
 public enum EditType {
-    OPEN_EDIT_MENU, QUICK_WITHDRAW, QUICK_DEPOSIT
+    OPEN_EDIT_MENU(false, false), QUICK_WITHDRAW(true, false),
+    QUICK_DEPOSIT(true, false), DEPOSIT_STOCK(true, false),
+    WITHDRAW_STOCK(true, false), DEPOSIT_BALANCE(true, false),
+    WITHDRAW_BALANCE(true, false), BUY_PRICE(true, false),
+    SELL_PRICE(true, false), SHOP_ITEM_AMOUNT(true, false),
+    PLAYER_BUY_LIMIT(true, false), PLAYER_SELL_LIMIT(true, false),
+    GLOBAL_BUY_LIMIT(true, false), GLOBAL_SELL_LIMIT(true, false),
+    APPEARANCE_CHANGE(true, true);
+
+    private final boolean hasAmount, hasStringValue;
+
+    EditType(boolean hasAmount, boolean hasStringValue) {
+        this.hasAmount = hasAmount;
+        this.hasStringValue = hasStringValue;
+    }
+
+    public boolean hasAmount() {return hasAmount;}
+
+    public boolean hasStringValue() {return hasStringValue;}
 }
--- src/main/java/xzot1k/plugins/ds/api/events/ShopEditEvent.java
+++ src/main/java/xzot1k/plugins/ds/api/events/ShopEditEvent.java
@@ -15,46 +15,66 @@ import xzot1k.plugins.ds.api.objects.Shop;
 public class ShopEditEvent extends Event implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private final Player player;
-    private final Shop shop;
-    private final EditType editType;
+    private Shop shop;
+    private EditType editType;
+    private double amount;
+    private String value;
     private boolean cancelled;
 
     public ShopEditEvent(@NotNull Player player, @NotNull Shop shop, @NotNull EditType editType) {
         this.player = player;
-        this.shop = shop;
-        this.editType = editType;
+        setShop(shop);
+        setEditType(editType);
+        setAmount(amount);
+        setValue(null);
+        setCancelled(false);
+    }
+
+    public ShopEditEvent(@NotNull Player player, @NotNull Shop shop, @NotNull EditType editType, double amount) {
+        this.player = player;
+        setShop(shop);
+        setEditType(editType);
+        setAmount(amount);
+        setValue(null);
+        setCancelled(false);
+    }
+
+    public ShopEditEvent(@NotNull Player player, @NotNull Shop shop, @NotNull EditType editType, String value) {
+        this.player = player;
+        setShop(shop);
+        setEditType(editType);
+        setAmount(0);
+        setValue(value);
         setCancelled(false);
     }
 
     // getters & setters
-    public static HandlerList getHandlerList() {
-        return handlers;
-    }
+    public static HandlerList getHandlerList() {return handlers;}
 
     @Override
-    public @NotNull HandlerList getHandlers() {
-        return handlers;
-    }
+    public @NotNull HandlerList getHandlers() {return handlers;}
 
     @Override
-    public boolean isCancelled() {
-        return cancelled;
-    }
+    public boolean isCancelled() {return cancelled;}
 
     @Override
-    public void setCancelled(boolean cancelled) {
-        this.cancelled = cancelled;
-    }
+    public void setCancelled(boolean cancelled) {this.cancelled = cancelled;}
 
-    public Player getPlayer() {
-        return player;
-    }
+    public Player getPlayer() {return player;}
 
-    public Shop getShop() {
-        return shop;
-    }
+    public Shop getShop() {return shop;}
 
-    public EditType getEditType() {
-        return editType;
-    }
+    public void setShop(Shop shop) {this.shop = shop;}
+
+    public EditType getEditType() {return editType;}
+
+    public void setEditType(EditType editType) {this.editType = editType;}
+
+    public double getAmount() {return amount;}
+
+    public void setAmount(double amount) {this.amount = amount;}
+
+    public String getValue() {return value;}
+
+    public void setValue(String value) {this.value = value;}
 }
