6c05baae0143c7ff1c124566e08ce86729b06252
XZot1K <alexosborne54@gmail.com>
2023-09-04 21:39:50 -0400

    **Build 22** * Added most actions to the "EditType" enumeration. * Reworked a majority of the "log.txt". Most actions will display the string form of the item modified. * Resolved Appearance loading issue.
    Signed-off-by: Jeremiah Osborne <alexosborne54@gmail.com>

--- src/main/java/xzot1k/plugins/ds/api/enums/EditType.java
+++ src/main/java/xzot1k/plugins/ds/api/enums/EditType.java
@@ -7,12 +7,17 @@ package xzot1k.plugins.ds.api.enums;
 public enum EditType {
     OPEN_EDIT_MENU(false, false), QUICK_WITHDRAW(true, false),
     QUICK_DEPOSIT(true, false), DEPOSIT_STOCK(true, false),
-    WITHDRAW_STOCK(true, false), DEPOSIT_BALANCE(true, false),
-    WITHDRAW_BALANCE(true, false), BUY_PRICE(true, false),
-    SELL_PRICE(true, false), SHOP_ITEM_AMOUNT(true, false),
-    PLAYER_BUY_LIMIT(true, false), PLAYER_SELL_LIMIT(true, false),
-    GLOBAL_BUY_LIMIT(true, false), GLOBAL_SELL_LIMIT(true, false),
-    APPEARANCE_CHANGE(true, true);
+    WITHDRAW_STOCK(true, false), STOCK_SET(true, false),
+    DEPOSIT_BALANCE(true, false), WITHDRAW_BALANCE(true, false),
+    BUY_PRICE(true, false), SELL_PRICE(true, false),
+    SHOP_ITEM_AMOUNT(true, false), PLAYER_BUY_LIMIT(true, false),
+    PLAYER_SELL_LIMIT(true, false), GLOBAL_BUY_LIMIT(true, false),
+    GLOBAL_SELL_LIMIT(true, false), APPEARANCE_CHANGE(false, true),
+    ASSISTANT_ADD(false, true), ASSISTANT_REMOVE(false, true),
+    DESCRIPTION_CHANGE(false, true), CLEAR_LIMITS(false, false),
+    DELETE(false, false), CLEAR_SALE_ITEM(false, true),
+    CLEAR_TRADE_ITEM(false, true), CURRENCY_CHANGE(false, true),
+    COMMAND_ADD(false, true), COMMAND_REMOVE(true, false);
 
     private final boolean hasAmount, hasStringValue;
 
